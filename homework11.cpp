e11d9688c77127c96f18543e8983781431dd7a3f
zhp006 <zhp006@ucsd.edu>
2021-03-07 11:47:18 -0800

    Finished implementing getAllSingleMoves for king piece

--- homework11.cpp
+++ homework11.cpp
@@ -6,6 +6,27 @@
 using namespace std;
 
 
+class Move{
+    public:
+        int sx;
+        int sy;
+        int dx;
+        int dy;
+        string moveType;
+        vector<pair<int, int>> skipped;
+
+        Move(int source_x, int source_y, int dest_x, int dest_y, string mType)
+        {
+            sx = source_x;
+            sy = source_y;
+            dx = dest_x;
+            dy = dest_y;
+            moveType = mType;
+        }
+
+
+};
+
 class Game
 {
     public:
@@ -13,12 +34,11 @@ class Game
         string mode;
         double timeLeft;
         vector<vector<char>> board;
-
+        vector<vector<bool>> visited;
 
         /* Parse the input file */
         void parse(string file)
         {
-            cout << "called" << endl;
             ifstream input;
             input.open(file);
             string line;
@@ -46,6 +66,86 @@ class Game
                 board.push_back(row);
             }
         }
+
+
+        /* Evalutate the board */
+        double eval()
+        {
+            double score = 0.0;
+            for(auto row : board)
+            {
+                for(auto p : row)
+                {
+                    if(p == 'w')
+                        score += 1.0;
+                    else if(p == 'W')
+                        score += 1.5;
+                    else if(p == 'b')
+                        score -= 1.0;
+                    else if(p == 'B')
+                        score -= 1.5;
+                }
+            }
+
+            return score;
+        }
+
+        /* Get all valid moves for a piece*/
+        vector<Move*> getAllMoves(int row, int col, bool isKing, vector<Move*> moves)
+        {
+            vector<Move*> allMoves;
+            
+
+
+            return allMoves;
+
+        }
+
+        /* Get all single move for a piece*/
+        vector<Move*> getAllSingleMoves(int row, int col, bool isKing)
+        {
+            vector<Move*> moves;
+            if(isKing)
+            {
+                vector<vector<int>> dir{{-1, -1}, {1, -1}, {1, 1}, {-1, 1}};
+                for(auto d : dir)
+                {
+                    int dest_x = col + d[0];
+                    int dest_y = row + d[1];
+
+                    if(dest_x >= 0 && dest_x < board.size() && dest_y >= 0 && board.size())
+                    {
+                        if(board[dest_y][dest_x] == '.' || true)
+                        {
+                            Move* m = new Move(col, row, dest_x, dest_y, "E");
+                            moves.push_back(m);
+                        }
+                    }
+                }
+            }
+            else if(player == "Black")
+            {
+                return moves;
+            }
+            else
+            {
+                return moves;
+            }
+
+            return moves;
+        }
+
+
+        /* DEBUG print board */
+        void printBoard()
+        {
+            for(auto row : board)
+            {
+                for(auto p : row)
+                    cout << p << " ";
+                cout << endl;
+            }
+        }
 };
 
 
@@ -54,16 +154,11 @@ int main()
     Game game;
     game.parse("input.txt");
 
-    cout << "game mode: " << game.mode << endl;
-    cout << "game player: " << game.player << endl;
-    cout << "time left: " << game.timeLeft << endl;
-    cout << "------------------------------------" << endl;
 
-    for(auto row : game.board)
-    {
-        for(auto p : row)
-            cout << p << " ";
-        cout << endl;
-    }
+    auto moves = game.getAllSingleMoves(0,0, true);
+    for(auto m : moves)
+        game.board[m->dy][m->dx] = '*';
+    
+    game.printBoard();
     return 0;
 }
